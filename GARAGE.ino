c2c8459653f2d8916a6b947a70667169cdc5f367
Marty Kokes <mkokes@gmail.com>
2016-01-15 23:27:50 -0600

    Split temp publishing and acquiring into separate timers. Refactored code a bit.

--- GARAGE.ino
+++ GARAGE.ino
@@ -4,17 +4,17 @@
 // DHT parameters
 #define DHTTYPE  DHT22              // Sensor type DHT11/21/22/AM2301/AM2302
 #define DHTPIN   D1         	      // Digital pin for communications
-#define DHT_SAMPLE_INTERVAL   2000  // Sample every two seconds
+#define DHT_SAMPLE_INTERVAL   2000  // How often to sample in milliseconds
 
 // Use uFL External Antenna
 STARTUP(WiFi.selectAntenna(ANT_EXTERNAL));
 
 // Variables
-int sampInt = 5000; //sample interval in milliseconds
+int lastPub;
+int pubDelay = 5000; // How often to publish in milliseconds
 int tempf;
-int temperature;
 int humidity;
-int targettemp = 70;
+int targettemp = 60;
 int blue = D7;
 int garageDoorRelay = D5;
 int furnaceRelay = D6;
@@ -47,7 +47,7 @@ void setup() {
   pinMode(furnaceRelay, OUTPUT);
 
   // Variables Exposed to Cloud
-  Particle.variable("temperature", temperature);
+  Particle.variable("temperature", tempf);
   Particle.variable("targettemp", targettemp);
   Particle.variable("humidity", humidity);
   Particle.variable("furnaceheat", furnaceHeat);
@@ -71,56 +71,48 @@ void dht_wrapper() {
 
 void loop()
 {
-  //Wait a bit between loops then grab the temperature
-  delay(sampInt);
-  temperature = tempf;
-
-  // Turn the furnace on or off if necessary
-  heatGarage();
-
   // Check if we need to start the next sample
   if (millis() > DHTnextSampleTime) {
+
     if (!bDHTstarted) {		// start the sample
       DHT.acquire();
       bDHTstarted = true;
+      digitalWrite(blue, HIGH);
     }
 
     if (!DHT.acquiring()) {		// has sample completed?
-
-      // get DHT status
-      //int result = DHT.getStatus();
       humidity = DHT.getHumidity();
       tempf = DHT.getFahrenheit();
-      //int tempc = DHT.getCelsius();
-      //int tempk = DHT.getKelvin();
-      //int dp = DHT.getDewPoint();
-      //int dpslow = DHT.getDewPointSlow();
 
-
-      //Turn the built in LED on to indicate publishing
-      digitalWrite(blue, HIGH);
-
-      //* Publish readings - uncomment/comment the values you'd like to publish
-
-      Particle.publish("Humidity", String(humidity) + "%");
-      Particle.publish("Temperature", String(tempf) + " °F");
-      //Particle.publish("Temperature", String(tempc) + " °C");
-      //Particle.publish("Temperature", String(tempk) + " °K");
-      //Particle.publish("Dew Point", String(dp) + " %");
-      //Particle.publish("Dew Point", String(dpslow) + " %");
-
-      //Turn the built in LED off indicate publishing finished
-      delay(250);
-      digitalWrite(blue, LOW);
+      // Turn the furnace on or off if necessary
+      heatGarage();
 
       n++;  // increment counter
       bDHTstarted = false;  // reset the sample flag so we can take another
       DHTnextSampleTime = millis() + DHT_SAMPLE_INTERVAL;  // set the time for next sample
     }
   }
+
+  if ( (millis() - lastPub) > pubDelay) {
+      blinkPub();
+      lastPub = millis();
+  }
+
 }
 
-// After Loop - POST functions
+int blinkPub() {
+      //Turn the built in LED on to indicate publishing
+      digitalWrite(blue, HIGH);
+
+      //Publish readings - uncomment/comment the values you'd like to publish
+      Particle.publish("Humidity", String(humidity) + "%");
+      Particle.publish("Temperature", String(tempf) + " °F");
+
+      //Turn the built in LED off indicate publishing finished
+      digitalWrite(blue, LOW);
+}
+
+// POST functions
 // These  function automagically get called upon a matching POST request
 int garageDoor(String command)
 {
@@ -134,11 +126,11 @@ int garageDoor(String command)
 int furnOnOff(String command)
 {
   if (command == "on") {
-    //furnaceOn();
+    furnaceOn();
     return 1;
   }
   if(command == "off") {
-    //furnaceOff();
+    furnaceOff();
     return 1;
   }
   return -1;
@@ -158,27 +150,31 @@ int activateGarageDoor() {
     digitalWrite(garageDoorRelay, LOW);
     delay(250);
     digitalWrite(garageDoorRelay, HIGH);
+    Particle.publish("Garage Door", "Active");
     return 1;
 }
 int heatGarage() {
-    if (targettemp >= temperature) {
-        if (furnaceActive = true) {
+    if (furnaceActive) {
+        if (targettemp >= tempf) {
             digitalWrite(furnaceRelay, LOW);
             furnaceHeat = 1;
+            Particle.publish("Furnace", "On");
             return 1;
         } else {
             digitalWrite(furnaceRelay, HIGH);
             furnaceHeat = 0;
+            Particle.publish("Furnace", "Off");
             return -1;
         }
     } else {
         digitalWrite(furnaceRelay, HIGH);
         furnaceHeat = 0;
+        Particle.publish("Furnace", "Off");
         return -1;
     }
 }
 int setFurnaceTemp() {
-    targettemp = 70;
+    targettemp = 60;
     return 1;
 }
 int furnaceOn() {
